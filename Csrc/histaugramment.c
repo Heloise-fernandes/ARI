523bc97c0176289d966c83b06a515ac1e8912b36
KyZoar <KyZoar@users.noreply.github.com>
2017-04-15 14:15:34 +0200

    Update histaugramment.c

--- Csrc/histaugramment.c
+++ Csrc/histaugramment.c
@@ -18,45 +18,6 @@
 
 int main(int argc, char *argv[])
 {
-	//cat ./mapping/2008_000001.sift | sort -n|uniq -c >testClusterShell.txt 
-	  //float monHisto[256];
-	  //int indice = 0;
-	  //unhistoDeClasse("./mapping/2008_000001.sift", 256, monHisto);
-	  /*int indice =0;
-	  int tailleMAXLu = 5;
-	  char chaine[tailleMAXLu];
-	
-	  for(indice=0; indice <256; indice++)
-	  {
-		 monHisto[indice]=0.0;
-	  }
-	
-	  FILE* f = fopen("./mapping/2008_000001.sift","r");
-	  int t=0;
-	  if(f)
-	  {  
-		while (fgets(chaine, tailleMAXLu, f) != NULL)
-		{
-			indice = atoi(chaine);
-			//printf("%d %s\n",indice,chaine);
-			monHisto[indice]++;
-			t++;
-		}
-		
-		printf("t = %d\n", t);
-		for(indice=0; indice <256; indice++)
-		{
-			monHisto[indice]=monHisto[indice]/t;
-			printf("%f\n",monHisto[indice]);
-		}
-	  }
-	  printf("\n");
-	  fclose(f);
-	*/
-	/*for(indice=0; indice <256; indice++)
-	{
-		printf("%f\n",monHisto[indice]);
-	}*/
 		
 	if(argc==2)
 	{
@@ -87,7 +48,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("Il manque un argument. Le programme se lance de deux maniËre diffÈrente :\n histo <fichier_Requete> <fichier_sift>\n histo {1,2}, permet de chrger le fichier des hitogramme de couleur(1) ou de cluster(2)\n");
+		printf("Il manque un argument. Le programme se lance de deux mani√®re diff√©rente :\n histo <fichier_Requete> <fichier_sift>\n histo {1,2}, permet de chrger le fichier des hitogramme de couleur(1) ou de cluster(2)\n");
 	}
 	exit(0);
 }
